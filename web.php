001264f704269cbb1051664bdb3c8754b98d10cf
octopus <torres@DESKTOP-QML9V1U.localdomain>
2021-10-04 16:09:58 +0900

    課題

--- web.php
+++ web.php
@@ -24,7 +24,8 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['prefix' => 'admin'],function() {
-  Route::get('news/create','Admin\NewsController@add');
+  Route::get('profile/create','Admin\ProfileController@add');
+  Route::get('profile/edit','Admin\ProfileController@edit');
 });
 
 Route::get('','Admin\AAAController@bbb');
